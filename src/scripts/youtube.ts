7ce4498acb5477820889ec33bca99792f7f58258
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-05-28 02:44:32 -0300

    fix: issues on load the app when change video

--- src/scripts/youtube.ts
+++ src/scripts/youtube.ts
@@ -1,39 +1,64 @@
-import { inject, remove } from "./injection"
+import { inject, remove } from './injection'
 
 let miniPlayerObserver: MutationObserver | null = null
+let videoObserver: MutationObserver | null = null
 
-const miniPlayerMutationHandler = (mutations: MutationRecord[]) => {
-  const watchOnPageAttribute = mutations.find(
-    (mutation) => mutation.attributeName == 'is-watch-page'
-  )
+const injectApp = () => {
+  const head = document.querySelector('head') as HTMLElement
+  const player = document.querySelector('#movie_player') as HTMLElement
+  inject(head, player)
+}
 
-  if (!watchOnPageAttribute) {
-    return
-  }
-
-  const miniPlayer = watchOnPageAttribute.target as HTMLElement
+const miniPlayerMutationHandler = () => {
+  const miniPlayer = document.querySelector('ytd-miniplayer') as HTMLElement
   const isWatchingOnPage = miniPlayer.hasAttribute('is-watch-page')
 
   if (!isWatchingOnPage) {
     remove()
     return
   }
-  const head = document.querySelector("head") as HTMLElement
-  const player = document.querySelector("#movie_player") as HTMLElement
 
-  inject(head, player)
+  injectApp()
+}
+
+const videoMutationHandler = () => {
+  remove()
+  injectApp()
 }
 
 const observeYoutubePlayerPosition = () => {
   miniPlayerObserver = new MutationObserver(miniPlayerMutationHandler)
   const miniPlayer = document.querySelector('ytd-miniplayer') as HTMLElement
+  
+  if (!miniPlayer) {
+    console.error('Mini player not found')
+    return
+  }
+
   miniPlayerObserver.observe(miniPlayer, {
     attributes: true,
+    attributeFilter: ['is-watch-page'],
+  })
+}
+
+const observeVideoChanged = () => {
+  videoObserver = new MutationObserver(videoMutationHandler)
+  const video = document.querySelector('video') as HTMLVideoElement
+
+  if (!video) {
+    console.error('Video not found')
+    return
+  }
+
+  videoObserver.observe(video, {
+    attributes: true,
+    attributeFilter: ['src'],
   })
 }
 
 const startObservation = () => {
   observeYoutubePlayerPosition()
+  observeVideoChanged()
 }
 
 startObservation()
