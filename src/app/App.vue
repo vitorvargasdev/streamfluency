2cd7c658bbeef1d039797335cf2282484b95d1df
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-06-08 17:36:35 -0300

    fix: positioning issues

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AppPanel from './components/appPanel/AppPanel.vue';
+import AppPanel from '@/app/components/appPanel/AppPanel.vue'
 </script>
 
 <template>
@@ -12,14 +12,7 @@ import AppPanel from './components/appPanel/AppPanel.vue';
 .app {
   margin: 0;
   padding: 0;
-  width: 100%;
-  height: 100vh;
   background: none;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 10;
   pointer-events: none;
 }
 </style>
-
--- src/app/components/subtitleDisplay/SubtitleDisplay.vue
+++ src/app/components/subtitleDisplay/SubtitleDisplay.vue
@@ -1,12 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const nativeSubtitle = ref('lorem ipsum')
+const learningSubtitle = ref('lorem ipsum')
+</script>
+
 <template>
   <div class="subtitle">
-    <div class="subtitle__primary subtitle__text">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-    </div>
-
-    <div class="subtitle__secondary subtitle__text">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-    </div>
+    <div class="subtitle__primary subtitle__text" v-html="nativeSubtitle" />
+    <div v-if="learningSubtitle" class="subtitle__secondary subtitle__text" v-html="learningSubtitle" />
   </div>
 </template>
 
