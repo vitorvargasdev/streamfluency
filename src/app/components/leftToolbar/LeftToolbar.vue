29964fe1c85dfc9f612d5606e6d5b6bd2c8ca8cb
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-06-08 22:06:45 -0300

    feat: add side buttons

--- src/app/components/leftToolbar/LeftToolbar.vue
+++ src/app/components/leftToolbar/LeftToolbar.vue
@@ -1,5 +1,11 @@
 <template>
-  <div style="background-color: blue;">
-    LEFT SIDE
+  <div class="side-button">
+    <img class="side-button__icon" src="@/app/assets/images/right-arrow.svg" />
+    <img class="side-button__icon" src="@/app/assets/images/reload.svg" />
+    <img class="side-button__icon" src="@/app/assets/images/left-arrow.svg" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@forward '@/app/assets/scss/side-buttons';
+</style>
--- src/app/components/rightToolbar/RightToolbar.vue
+++ src/app/components/rightToolbar/RightToolbar.vue
@@ -1,5 +1,11 @@
 <template>
-  <div style="background-color: pink;">
-    RIGHT SIDE
+  <div class="side-button">
+    <img class="side-button__icon" src="@/app/assets/images/texts.svg" />
+    <img class="side-button__icon" src="@/app/assets/images/languages.svg" />
+    <img class="side-button__icon" src="@/app/assets/images/settings.svg" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@forward '@/app/assets/scss/side-buttons';
+</style>
